e66500c0818935be45948382e7f7e24da8ca65cf
lbrh <s4051927@student.rmit.edu.au>
2025-09-25 00:05:40 +1000

    now should work :)

--- next.config.ts
+++ next.config.ts
@@ -8,8 +8,6 @@ const nextConfig: NextConfig = {
   images: {
     unoptimized: true
   },
-  basePath: process.env.NODE_ENV === 'production' ? '/masscubed.com' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/masscubed.com' : '',
 };
 
 export default nextConfig;
