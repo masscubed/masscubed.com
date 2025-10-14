11da6cde18942a2ec360bcf89f3917f1ab8756b3
lbrh <s4051927@student.rmit.edu.au>
2025-10-14 11:37:58 +1100

    "fixed" events not listing (was defaulting to manually entered events)

--- src/pages/events.tsx
+++ src/pages/events.tsx
@@ -102,7 +102,7 @@ export default function Events({ events, error }: EventsPageProps) {
 
 // If google calander isn't working or in use anymore, update this
 const getEvents = (): Event[] => [
-  { // copy and paste this template including { and } below to add more events
+  /* { // copy and paste this template including { and } below to add more events
     id: '3dgfccehq0mvaps7r5ntf114i4@google.com', // google calendar event id
     title: 'Title', // Event title
     description: '<p>If you plan to attend, please fill out this form <a href="https://docs.google.com/forms/d/e/1FAIpQLSc4jJqE5VJGIYaXch1IY8k0f6XfLj7EuTYuk5WMsSFNuR2p-g/viewform?usp=dialog"><u>here</u></a>.</p>', // Event description, can include links
@@ -112,6 +112,7 @@ const getEvents = (): Event[] => [
     htmlLink: '', // Link to the event on Google Calendar
     isAllDay: false, // Whether the event is an all-day event
   },
+  */
 ];
 
 export const getStaticProps: GetStaticProps<EventsPageProps> = async () => {
