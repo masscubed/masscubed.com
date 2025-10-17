2018fd7bdf3c753a2add6c83d3db8df171a8792e
lbrh <s4051927@student.rmit.edu.au>
2025-10-17 22:26:32 +1100

    horsing it

---
+++ src/pages/battleofthehorses.tsx
@@ -0,0 +1,16 @@
+import Layout from '../components/Layout';
+
+export default function BattleOfTheHorses() {
+  return (
+    <Layout title="MASS Cubed—Battle of the Horses" description="Battle of the Horses">
+      <div className="horsing">
+        <div className="horsetext">
+          <h1 className="text-center">Battle of the Horses</h1>
+          <h2 className="text-center">
+            Oh yeah battle of the horses oh yeah look at those horses oh yeah they be battling
+          </h2>
+        </div>
+      </div>
+    </Layout>
+  );
+}
