e374d54145035501e31a648bde83402c891292d0
ntoo0005 <ntoo0005@student.monash.edu>
2025-10-22 18:33:41 +1100

    trying again

--- src/pages/committee.tsx
+++ src/pages/committee.tsx
@@ -13,7 +13,7 @@ export default function Committee() {
           <CommitteeMember
               name="Isabella Ferguson (she/her)"
               role="President"
-              bio="    Hello! I'm Isabella, and I'm so exited to be your 2026 President!! I'm in the Advanced 
+              bio=" Hello! I'm Isabella, and I'm so exited to be your 2026 President!! I'm in the Advanced 
                Research degree, and majoring in chemistry and earth science. I'm super passionate about all
                sorts of nerdy science especially science communication, and love getting hands on and doing
                things with real word applications. You can usually find me floating around commo between classes,
