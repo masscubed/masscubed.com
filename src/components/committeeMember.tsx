8166c7c0b541b21b68552cc4d762258ce0d13d72
ntoo0005 <ntoo0005@student.monash.edu>
2025-10-22 17:17:24 +1100

    fixing image

--- src/components/committeeMember.tsx
+++ src/components/committeeMember.tsx
@@ -9,23 +9,23 @@ interface CommitteeMemberProps {
 
 export default function CommitteeMember({name, role, bio, imageUrl}: CommitteeMemberProps) {
     return (
-        <div className="committee-member">
+        <div className="committee-member flex gap-6 items-start mb-8">
             {imageUrl && (
-                <div className="profile-image">
+                <div className="profile-image flex-shrink-0">
                     <Image
                         src={imageUrl}
                         alt={`${name} - ${role}`}
                         width={150}
                         height={150}
-                        className="rounded-full"
+                        className="rounded-full object-cover"
+                        style={{ aspectRatio: '1/1' }}
                     />
                 </div>
             )}
-            <div className="member-info">
+            <div className="member-info flex-grow">
                 <b>{role}</b> {name}<br/>
                 <p><em>{bio}</em></p>
             </div>
-            <br/>
         </div>
     )
 }
