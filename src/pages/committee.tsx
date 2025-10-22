67c2069e486910afa087a87565674f017bb723a5
ntoo0005 <ntoo0005@student.monash.edu>
2025-10-22 16:54:58 +1100

    updating some bios for 2026 committee

--- src/pages/committee.tsx
+++ src/pages/committee.tsx
@@ -7,79 +7,68 @@ export default function Committee() {
     <Layout title="MASS Cubed—Committee" description="Monash Advanced Science & Science Scholars Society">
       <main>
         <header className="bottom-horizontal">
-          <h1>Committee of 2024-2025</h1>
+          <h1>Committee of 2026</h1>
         </header>
         <div>
           <CommitteeMember
-              name="Caitlyn Smith (she/her)"
+              name="Isabella Ferguson (she/her)"
               role="President"
-              bio="Hello!!
-            My name is Caitlyn (she/her) and I am super excited to be your 2025 President!!
-            I am in the Advanced Research degree and majoring in maths and physics. I enjoy board
-            games, abstract celebrity heads and talking to people in commo.
-            I always love meeting new people and getting to know them, so come say hi!!"
+              bio=""
           />
           <CommitteeMember
               name="Leith Murray (he/him)"
               role="Vice President"
-              bio="Howdy! I'm Leith and I have the delight of being your Vice President for 2025. 
-            I'm doing a double major of Earth Science and Chemistry in the Advanced Research degree. 
+              bio="Howdy! I'm Leith and I have the delight of being your Vice President for 2026. 
+            I'm doing my Honours in Environmental Chemistry in the Advanced Research degree. 
             Always free for a chat or to answer any questions, I can often be found taking samples in 
             your nearest mangrove, marsh, or bog."
           />
           <CommitteeMember
-              name="Siena Izzard (she/her)"
-              role="Secretary"
-              bio="Hi! I'm Siena— your 2025 Secretary. I'm a physics and maths double major in the Advanced 
-            Research degree, so most days you can find me in the common room starting at a whiteboard full 
-            of integrals. Feel free to come have a chat about uni, MASS3 or my small army of houseplants."
-          />
-          <CommitteeMember
-              name="Dhairya Hans (she/her)"
-              role="Treasurer"
-              bio="Hi! My name is Dhairya, and I am the super excited to be the treasurer 
-            for 2024/2025. I am absolutely honoured and delighted to be chosen for this role! I'm a 
-            second year research student majoring in chemistry, and minoring in maths. I'd love to get 
-            to know all of you better, so if you see me, don't hesitate to come say hi!"
-          />
-          <CommitteeMember
               name="Zach Peeters (he/him)"
-              role="Assistant Treasurer"
-              bio="Hi, I'm Zach, your first assistant treasurer! I'm a GC student, hopefully majoring in ecology, 
+              role="Secretary"
+              bio="Hi, I'm Zach, your Secretary! I'm a GC student, hopefully majoring in ecology, 
             but I also love languages and learning about all the crazy discoveries the other scientific disciplines 
             are making. If you need to find me, I live in the common room. Drop by commo on a Wednesday morning 
             and I would love to share a cup of tea (or coffee) with you!"
           />
           <CommitteeMember
-              name="Jack Travers (he/they)"
+              name="Nick Missen (he/him)"
+              role="Treasurer"
+              bio="Hi! I'm Nick, and I'll be your Treasurer in 2026!! I'm a research student 
+            studying Astrophysics, Physics, and Maths, so you'll probably find me in commo crying over the 
+            work I don't understand. Please drop in and say hi . . . for my sake."
+          />
+          <CommitteeMember
+              name="Ivy Lai (she/her)"
+              role="Assistant Treasurer"
+              bio="Hey there, I'm Ivy, the second ever Assistant Treasurer! I'm an Advanced Research student, 
+              majoring in pure maths and astrophysics. I play netball and study Japanese in my spare 
+              time, and am always happy to have a chat! :)"
+          />
+          <CommitteeMember
+              name="Kayla Quinn (she/her)"
               role="Education Officer"
-              bio="Hey, I'm Jack, and I'm super excited to be your Education Officer for 2025! 
-            I'm an Advanced Research student, extended-majoring in maths and hoping to go into meteorology 
-            (although my MonPlan is ever changing). I'd always love to hear from you so reach out at any time"
+              bio=""
           />
           <CommitteeMember
-              name="Elaine Marques (she/her)"
-              role="Events Coordinator"
-              bio="Hi, I'm Elaine and I'm privileged to be your Events Coordinator for 2025! 
-            I'm enrolled in the Advanced Research degree and am completing an extended major in 
-            chemistry. I'm probably one of the biggest chemistry enthusiasts in the science 
-            precinct and I'm frequently around the advanced science common room. I'm always 
-            free for a chat and please feel free to reach out to me if you have any queries or 
-            suggestions regarding events too!"
-          />
-          <CommitteeMember
-              name="Michael Cotugno (he/him)"
+              name="Jacinta Tait (she/her)"
               role="Marketing Officer"
-              bio="I'm Michael, unofficial face of the 2023 MASS Cubed Instagram and proud new editor
-             for Mass Cubed's promotion and outreach. Next year, you can expect bad jokes, electronic music, 
-             and environmental policy to all work their way into my role"
+              bio="Hi! I'm Jacinta and I'm the Marketing Officer for 2026. I'm an advanced research student planning to 
+              do a major in ecology and a second major in physiology. If you couldn't tell from my choice of majors,
+              I love all things biology. When I'm not doing school work, I love to read, crochet or play video games. 
+              If you see me around, feel free to ask me any questions or just have a chat with me."
           />
           <CommitteeMember
-              name="Isabella Ferguson (she/her)"
+              name="Niamh Toohey (she/her)"
               role="Communication Officer"
-              bio="Hi!! I'm Isabella, and I'm really excited to be the communications officer for 2025! I'm studying 
-            advanced research, and majoring in chemistry and earth science. I love getting to know people, and am always searching for new things to put in the newsletter or up in commo,
-            so come have a chat!!"
+              bio="Hello! I'm Niamh and I'm very excited to be your Communications Officer for 2026! 
+              I'm studying advanced research, majoring in Climate and Atmospheric Science, with a minor in Physics and Maths. 
+              I love to knit and all things Antarctica and I'm always happy to chat!"
+          />
+                    <CommitteeMember
+              name="TBD"
+              role="Events Coordinator"
+              bio=""
           />
           <CommitteeMember
               name="TBD"
